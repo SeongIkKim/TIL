db947ea1ff9dcc8b83055b3cbdd963c017da43c7
SeongIkKim <kpic1638@gmail.com>
2020-04-19 01:03:38 +0900

    [CREATE] DataStructure lab_5 BST complete

--- DataStructure/lab_5/p5.c
+++ DataStructure/lab_5/p5.c
@@ -2,46 +2,139 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct TreeNode *treeptr;
-
 typedef struct TreeNode{
 	int value;
-	treeptr *left, *right;
-}TreeNode;
+	struct TreeNode *left;
+	struct TreeNode *right;
+}Node;
+
+Node *root = NULL;
 
 // 삽입
-TreeNode *InsertNode(int value, TreeNode *root){
-	
+Node *InsertNode(int value, Node *root, FILE *fp){
+	if (root==NULL){
+		root = (Node*)malloc(sizeof(Node));
+		root->left = root->right = NULL;
+		root->value = value;
+		// printf("insert %d\n",value);
+		return root;
+	}
+	else{
+		if(root->value > value){
+			root->left = InsertNode(value, root->left,fp);
+		}
+		else if(root->value < value){
+			root->right = InsertNode(value, root->right,fp);
+		}
+		else{
+			fprintf(fp,"%d already exists.\n",value);
+		}
+	}
+	return root;
 };
 
 // left subtree중 최댓값을 찾아주는 함수
-TreeNode *FindMax(TreeNode *tree){
-	
+Node *FindMax(Node *root){
+	if (root==NULL){
+		return NULL;
+	}
+	Node *tempNode = root->left;
+	while(tempNode->right != NULL){
+		tempNode = tempNode->right;
+	}
+	return tempNode;
 };
 	
 // 삭제
-TreeNode *DeleteNode(int value, TreeNode *root){
+Node *DeleteNode(int value, Node *root, FILE *fp){
 	
+	Node *tempNode = NULL;
+	
+	if (root == NULL){
+		// 찾는 노드가 없을 때
+		fprintf(fp,"Deletion failed. %d does not exist.\n",value);
+		return NULL;
+	}
+	
+	// 해당 value를 찾아 내려가기
+	if (root->value > value){
+		root->left = DeleteNode(value, root->left,fp);
+	}
+	else if (root->value < value){
+		root->right = DeleteNode(value, root->right,fp);
+	}
+	else{	// root->value == value
+		// 자식 노드가 두개 있을 때
+		if (root->left != NULL && root->right != NULL){
+			tempNode = FindMax(root);
+			root->value = tempNode->value;	// 원래 root자리에 있던 value를 findMax value로 대체한다
+			root->left = DeleteNode(tempNode->value,root->left,fp);	 // root의 left subtree에서 해당 값을 가진 원래의 노드를 삭제한 뒤, left subtree 전체를 반환하여 다시 root->left에 할당한다
+			// root->left에 다시 재할당하는 이유를 명확히 이해하자.
+		}
+		else {
+			// 자식노드가 한개 있을때
+			if (root->left != NULL ){
+				tempNode = root->left;
+				free(root);
+			}
+			else if (root->right != NULL){
+				tempNode = root->right;
+				free(root);
+			}
+			else {
+				// 자식노드가 없을 때 (리프노드일때)
+				free(root);
+				// tempNode == NULL
+			}
+			return tempNode;
+		}
+		
+	}
 };
 
 // 탐색
-TreeNode *FindNode(int value, TreeNode *root){
-	
+Node *FindNode(int value, Node *root){
+	Node *tempNode = root;
+	while(tempNode != NULL){
+		if (tempNode->value > value){
+			tempNode = tempNode->left;
+		}
+		else if (tempNode->value < value){
+			tempNode = tempNode->right;
+		}
+		else{
+			return tempNode;
+		}
+	}
+	return NULL;
 };
 
 // 중위순회
-void PrintInorder(TreeNode *root){
-	
+void PrintInorder(Node *root, FILE *fp){
+	if (root != NULL){
+		PrintInorder(root->left,fp);
+		fprintf(fp,"%d ",root->value);
+		PrintInorder(root->right,fp);
+	}
 };
 
 // 전위순회
-void PrintPreorder(TreeNode *root){
-	
+void PrintPreorder(Node *root, FILE *fp){
+	if (root != NULL){
+		fprintf(fp,"%d ",root->value);
+		PrintPreorder(root->left,fp);
+		PrintPreorder(root->right,fp);
+	}
 };
 
 // 후위순회
-void PrintPostorder(TreeNode *root){
-
+void PrintPostorder(Node *root, FILE *fp){
+	if (root != NULL){
+		PrintPostorder(root->left,fp);
+		PrintPostorder(root->right,fp);
+		fprintf(fp,"%d ",root->value);
+		
+	}
 };
 
 int main(){
@@ -49,13 +142,14 @@ int main(){
 	int fileSize;
 	
 	FILE *fp1; // input.txt
-	FILE *fp2; // output.txt
+	FILE *fp2; // output.txx
 	
 	fp1 = fopen("input.txt","r");
 	if (fp1 == NULL){
 		fputs("FILE ERROR",stderr);
 		exit(1);
 	}
+	fp2 = fopen("output.txt","w");
 	
 	// 파일의 크기를 fileSize 에 저장한다.
 	fseek(fp1, 0, SEEK_END); // fp1을 열어 스트림의 위치지정자를 0부터 파일끝까지 이동시킨다
@@ -72,8 +166,8 @@ int main(){
 	// 그 파일의 내용을 버퍼에 저장한다.
   	fread(buffer, 1, fileSize, fp1);
 	
-	// printf("%s\n",buffer);
-	// printf("filesize = %d\n",fileSize);
+	// fprintf(fp2,"%s\n",buffer);
+	// fprintf(fp2,"filesize = %d\n",fileSize);
 	
 	char *instructionSet[30];
 	
@@ -88,13 +182,13 @@ int main(){
 			// 지금까지 들어온 것으로 한 instruction 구성
 			line[j] = '\0'; // 이전 instruction의 찌꺼기 buffer를 비우기
 			
-			// printf("지금라인:%s\n",line);
+			// fprintf(fp2,"지금라인:%s\n",line);
 			
 			// line을 instructionSet으로 동적할당
 			instructionSet[k] = (char*)malloc(sizeof(char)*(j-1));
 			strcpy(instructionSet[k],line); 
 			
-			// printf("%s\n",instructionSet[k]);
+			// fprintf(fp2,"%s\n",instructionSet[k]);
 			
 			k++;
 			line[0] = '\0'; // 이번 line의 버퍼를 비우기
@@ -109,6 +203,8 @@ int main(){
     char *order;
     char *valueStr;
     int value;
+	
+	Node *result = NULL; // 함수 실행 결과 리턴받은 노드를 저장해두는 포인터
 
 	i=0; // index
 	for(i=0; i<k ; i++){
@@ -117,8 +213,46 @@ int main(){
 		
 		if (valueStr != NULL){
 			value = atoi(valueStr);
-			printf("%d\n",value);
+			// printf("%d\n",value);
+		}
+	
+		
+		if (strcmp(order,"i") == 0){
+			root = InsertNode(value,root,fp2);
+		}
+		else if (strcmp(order,"d") == 0){
+			result = DeleteNode(value,root,fp2);
+		}
+		else if (strcmp(order,"f") == 0){
+			result = FindNode(value,root);
+			if (result == NULL){
+				fprintf(fp2,"%d is not in the tree.\n",value);
+			}
+			else{
+				fprintf(fp2,"%d is in the tree.\n",result->value);
+			}
+		}
+		else if (strcmp(order,"pi") == 0){
+			fprintf(fp2,"pi - ");
+			PrintInorder(root, fp2);
+			fprintf(fp2,"\n");
+		}
+		else if (strcmp(order,"pr") == 0){
+			fprintf(fp2,"pr - ");
+			PrintPreorder(root, fp2);
+			fprintf(fp2,"\n");
+		}
+		else if (strcmp(order,"po") == 0){
+			fprintf(fp2,"po - ");
+			PrintPostorder(root, fp2);
+			fprintf(fp2,"\n");
+		}
+		else{
+			fprintf(fp2,"order: %s\n",order);
 		}
 		
+		order[0] = '\0'; // order buffer 지우기
 	}
+	fclose(fp1);
+	fclose(fp2);
 }

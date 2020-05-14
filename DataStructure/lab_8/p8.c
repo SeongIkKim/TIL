467ab4fc3ef653500f0421853d1478134cd9abbb
SeongIkKim <kpic1638@gmail.com>
2020-05-14 17:02:53 +0900

    [CREATE] DataStructure lab_8 MaxHeap complete

--- DataStructure/lab_8/p8.c
+++ DataStructure/lab_8/p8.c
@@ -8,17 +8,49 @@ typedef struct HeapStruct{
 	int *Elements;
 }PriorityQueue;
 
-void Insert(int X,PriorityQueue *H,FILE *fp){
-	
+int isFull(PriorityQueue *H){
+	int result = H->Capacity == H->Size ? 1 : 0;
+	return result;
 }
 
 int Find(int X,PriorityQueue *H){
-	
+	int result=0;
+	int i =0;
+	for (i=1; i<=H->Size; i++){
+		if (H->Elements[i] == X){
+			result = 1;
+			return result;
+		}
+	}
+	return result;
 }
+
+void Insert(int X,PriorityQueue *H,FILE *fp){
+	int i;
+	if (isFull(H)){
+		fprintf(fp,"heap is full.\n");
+		return;
+	}
+	
+	if (Find(X,H)){
+		fprintf(fp,"%d is already in the tree.\n",X);
+		return;
+	}
+	
+	// percolating up
+	for (i=++H->Size; i/2 != 0 && H->Elements[i/2] < X ; i /= 2){
+		H->Elements[i] = H->Elements[i/2];
+	}
+	H->Elements[i] = X;
+	fprintf(fp,"insert %d\n",H->Elements[i]);
+	
+	return;
+}
+
 
 void Print(PriorityQueue *H,FILE *fp){
 	int i;
-	for (i=1; i< H->Capacity; i++){
+	for (i=1; i<= H->Size; i++){
 		fprintf(fp, "%d ",H->Elements[i]);
 	}
 }
@@ -91,51 +123,47 @@ int main(){
     char *valueStr;
     int value;
 	
-	// PriorityQueue *p_Queue;
-	// p_Queue->Capacity = 10;
-	// p_Queue->Size = 10;
-	// int e[10] = {90,36,26,25,19,17,1,7,3,2};
-	// p_Queue->Elements = e;
+	
 	
 	int result;
 	
-	PriorityQueue *p_Queue;
-	p_Queue->Capacity = atoi(instructionSet[0])+1; // 컴파일 오류. 생성자 포인터에 바로 할당안되는듯?
-	// p_Queue->Size = 0;
+	PriorityQueue *p_Queue = (PriorityQueue*)malloc(sizeof(PriorityQueue));
+	int initSize = atoi(instructionSet[0])+1;
+	p_Queue->Capacity = initSize-1;
+	p_Queue->Size = 0;
+	p_Queue->Elements = (int*)malloc(sizeof(int)*initSize);
 	
-	// printf("Elements: %d \n",p_Queue->Capacity);
 	
 	for(i=1; i<k ; i++){
-		printf("%s\n",instructionSet[i]);
-		// order = strtok(instructionSet[i]," ");
-		// valueStr = strtok(NULL, "\0");
+		order = strtok(instructionSet[i]," ");
+		valueStr = strtok(NULL, "\0");
 		
-		// if (valueStr != NULL){
-		// 	value = atoi(valueStr);
-		// 	// printf("%d\n",value);
-		// }
+		
+		if (valueStr != NULL){
+			value = atoi(valueStr);
+		}
 	
 		
-		// if (strcmp(order,"i") == 0){
-		// 	Insert(value,p_Queue,output);
-		// }
-		// else if (strcmp(order,"f") == 0){
-		// 	result = Find(value,p_Queue);
-		// 	if (!result){
-		// 		fprintf(output,"%d is not in the heap.\n",value);
-		// 	}
-		// 	else{
-		// 		fprintf(output,"%d is in the heap.\n",result);
-		// 	}
-		// }
-		// else if (strcmp(order,"p") == 0){
-		// 	Print(p_Queue,output);
-		// }
-		// else{
-		// 	fprintf(output,"order: %s\n",order);
-		// }
+		if (strcmp(order,"i") == 0){
+			Insert(value,p_Queue,output);
+		}
+		else if (strcmp(order,"f") == 0){
+			result = Find(value,p_Queue);
+			if (!result){
+				fprintf(output,"%d is not in the heap.\n",value);
+			}
+			else{
+				fprintf(output,"%d is in the heap.\n",value);
+			}
+		}
+		else if (strcmp(order,"p") == 0){
+			Print(p_Queue,output);
+		}
+		else{
+			fprintf(output,"order: %s\n",order);
+		}
 		
-		// order[0] = '\0'; // order buffer 지우기
+		order[0] = '\0'; // order buffer 지우기
 	}
 	fclose(input);
 	fclose(output);
